c5324a7a5d895d37fc1746cb33ec6dda635fd277
Dongdong Zhou <dzhou121@gmail.com>
2018-04-18 11:00:00 +0100

    ignore _build

--- firmware-left/custom/s130/armgcc/_build/nrf51422_xxac_RTT_Syscalls_GCC.c.d
+++
@@ -1,2 +0,0 @@
-_build/nrf51422_xxac_RTT_Syscalls_GCC.c.o: \
- ../../../../../../external/segger_rtt/RTT_Syscalls_GCC.c
--- firmware-left/custom/s130/armgcc/_build/nrf51422_xxac_SEGGER_RTT_printf.c.d
+++
@@ -1,2 +0,0 @@
-_build/nrf51422_xxac_SEGGER_RTT_printf.c.o: \
- ../../../../../../external/segger_rtt/SEGGER_RTT_printf.c
