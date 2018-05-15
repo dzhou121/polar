25ff86bfd79728da7ac650d834d13dda84773e34
Dongdong Zhou <dzhou121@gmail.com>
2018-05-15 06:24:17 +0100

    change right program.sh

--- firmware-right/program.sh
+++ firmware-right/program.sh
@@ -16,8 +16,6 @@ echo '============================= PROGRAMMING ============================='
 	sleep 0.1;
 	echo "nrf51 mass_erase";
 	sleep 1;
-	echo "flash write_image erase /Users/Lulu/nRF5_SDK_12/components/softdevice/s130/hex/s130_nrf51_2.0.1_softdevice.hex";
-	sleep 29;
 	echo "flash write_image /Users/Lulu/nRF5_SDK_12/examples/polar/firmware-right/custom/blank/armgcc/_build/nrf51422_xxac.hex";
 	sleep 10;
 	echo "reset";
